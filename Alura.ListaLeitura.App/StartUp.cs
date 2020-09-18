38e8dc3ba61ab0a9a4a650c75688fa31a815f361
Silvio Antônio de Oliveira Júnior <silvio.junior@crp.com>
2020-09-18 09:41:21 -0300

    Resposta padrao da requesição http

--- Alura.ListaLeitura.App/StartUp.cs
+++ Alura.ListaLeitura.App/StartUp.cs
@@ -1,10 +1,25 @@
-﻿namespace Alura.ListaLeitura.App
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
 {
     public class StartUp
     {
-       public void Configure() { }
+       public void Configure(IApplicationBuilder applicationBuilder) 
+        {
+            applicationBuilder.Run(LivrosParaLer);
+        }
 
-       
+        //Quando uma requisição for feita, um obj do HttpContext vira
+        //O Retorno deve ser Task (Paralelismo), pois o metodo 'Run()' aceita apenas esse tipo
+        //O WriteAsync() retorna o tipo task
+       public Task LivrosParaLer(HttpContext httpContext)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return httpContext.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
 
     }
 }
